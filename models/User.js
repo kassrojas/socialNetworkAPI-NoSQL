eaaf76b63e3688041d4ef3ac3e22e8191e294565
kassrojas <kassandrarojass@gmail.com>
2023-01-30 20:47:42 -0500

    added validate email function

--- models/User.js
+++ models/User.js
@@ -1,5 +1,11 @@
 const { Schema, model } = require('mongoose');
 
+const validateEmail = function (email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
+
 const userSchema = new Schema(
     {
         username: {
