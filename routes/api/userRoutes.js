7779b52406521fc5f893bf591193fbc7364cf7ff
kassrojas <kassandrarojass@gmail.com>
2023-01-31 21:17:21 -0500

    userRoutes.js and thoughtRoutes.js set up

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const {
+    getUsers,
+    createUser,
+    getSingleUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend,
+} = require('../../controllers/userController');
+
+// /api/user
+router.route('/').get(getUsers).post(createUser);
+
+// /api/user/:userId
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends
+router.route('/:userId/friends').post(addFriend);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').delete(removeFriend);
+
+module.exports = router;
